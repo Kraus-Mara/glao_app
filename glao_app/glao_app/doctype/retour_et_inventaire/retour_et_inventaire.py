8812c21492932b653a53475a601fc6647e285358
Kraus-Mara <antoinep.kraus@gmail.com>
2026-06-29 10:17:47 +0200

    Disable validate and _fetch_all_stuff methods
    Commented out the validate and _fetch_all_stuff methods to prevent their execution. (Currently preventing the installation on a shitty computer, due to the IT completely had shutdown the computer while i was working on it)

--- glao_app/glao_app/doctype/retour_et_inventaire/retour_et_inventaire.py
+++ glao_app/glao_app/doctype/retour_et_inventaire/retour_et_inventaire.py
@@ -19,38 +19,12 @@ class RetouretInventaire(Document):
         saved: DF.Check
         sent_items: DF.Table[RetourItems]
     # end: auto-generated types
-
-    def validate(self):
-        self.saved = 1
-        self._fetch_all_stuff()
-
-    def _fetch_all_stuff(self):
-        all_dmc = frappe.get_all(
-            "Gestion DMC",
-            filters=[["project", "=", self.project], ["status", "in", ["Validated", "Partially validated"]]],
-        )
-        items = []
-        compos = []
-        for d in all_dmc:
-            e = frappe.get_all("Gestion DMC Items", filters=[["parent", "=", d.name]])
-            for i in e:
-                r = frappe.get_doc("Gestion DMC Items", i.name)
-                items.append([r.item_from_stock, r.designation, r.true_quantity]) if r.reserved else None
-            for c in frappe.get_all("Gestion DMC Compositions", filters=[["parent", "=", d]]):
-                compos.append(c.composition) if c.quantity > 0 else None
-        # frappe.throw(str(items))
-        for r in items:
-            frappe.get_doc(doctype="Retour Items", item=r.item_from_stock, item_name=r.designation,
-                           sent_quantity=r.true_quantity)
-            self.sent_items.append("sent_items"
-                {
-                    "doctype": "Retour Items",
-                    "item": r.item_from_stock,
-                    "item_name": r.designation,
-                    "sent_quantity": r.true_quantity,
-                }
-            ).insert(ignore_permissions=True)
-
+	
+    # def validate(self):
+    #    self.saved = 1
+    #    self._fetch_all_stuff()
+	
+    
     # def _expedier(self):
     #   if self.status != "Draft":
     #       items = []
